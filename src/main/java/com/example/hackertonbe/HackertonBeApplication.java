337008bfe09b4eb5e7430c38f53b4cbe81c9ea47
holychan <east0321@outlook.kr>
2025-07-17 14:30:45 +0900

    Initial commit

---
+++ src/main/java/com/example/hackertonbe/HackertonBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackertonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackertonBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackertonBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackertonbe/HackertonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackertonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackertonBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
